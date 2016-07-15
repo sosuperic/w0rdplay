0b0e9fe2465ec297b0a611aa4df2b379d076c2b4
Eric Chu <eric.chu.56@gmail.com>
2016-07-14 21:13:14 -0400

    [Syllabizer] Catch extra case for syllabification
    Test word: 'apologized'
    
    The 'D' phoneme fell into the fifth case (consonant surrounded by non-vowels).
    However, it at the end, which caused a) an out of bounds error, and b) the while
    loop that continues until a vowel to be invalid (it never hits a vowel).
    
    Also split functions into helper functions so I could directly copy and
    paste into acoustic/feature_extractor.py. (Copying and pasting is pretty stupid,
    but I'm deliberately keeping these repos separate for now before I create a parent
    synthesis git repo that includes them all).

--- syllabizer.py
+++ syllabizer.py
@@ -15,45 +15,20 @@ class Syllabizer:
 	def is_vowel(self, phoneme):
 		return not set('AEIOU').isdisjoint(set(phoneme))
 
-	def phoneme_syllabize(self, s):
+	def count_number_of_syllables(self, s):
 		"""
-		Return phonemes grouped by syllable
-
-		Examples
-		--------
-		green -> [[u'G', u'R', u'IY1', u'N']]
-		hello -> [[u'HH', u'AH0'], [u'L', u'OW1']]
-		invisible -> [[u'IH0', u'N'], [u'V', u'IH1'], [u'Z', u'AH0'], [u'B', u'AH0', u'L']]
-		newspaper -> [[u'N', u'UW1', u'Z'], [u'P', u'EY2'], [u'P', u'ER0']]
-		explicit -> [[u'IH0', u'K'], [u'S', u'P', u'L', u'IH1'], [u'S', u'AH0', u'T']]
-		airplane -> [[u'EH1', u'R'], [u'P', u'L', u'EY2', u'N']]
-		idea -> [[u'AY0'], [u'D', u'IY1'], [u'AH0']]
-
-		Pseudocode
-		----------
-		If word has only one syllable, return phonemes
-
-		If current phoneme is vowel:
-			If previous phoneme is vowel:
-				this is start of new syllable 
-			Else (previous phoneme is consonant):
-				this is part of previous syllable
-
-		Else (current phoneme is consonant):
-			If next phoneme is vowel:
-		      	this is start of new syllable
-		  	Else if previous phoneme is vowel:
-		      	this is part of previous syllable
-		  	Else (both previous and next aren't vowels), e.g. airplane EH1 R *P* L EY2 N; scogin *S* K OW1 G IH0 N 
-				this is start of new syllable
-				append until next vowel
-
-		Notes
-		-----
-		This can be used to build the context for linguistic features for a synthesizer. While a word is split into
-		the basic unit of a phoneme, we want to know for every phoneme, which syllable, word, and phrase it belongs to. 
+		Return number of vowel phonemes
 		"""
+		phonemes = self.word_to_phonemes(s)
+		return self._count_number_of_syllables_in_phonemes(phonemes)
 
+	def _count_number_of_syllables_in_phonemes(self, phonemes):
+		return len([p for p in phonemes if self.is_vowel(p)])
+
+	def _phoneme_syllabize(self, phonemes):
+		"""
+		Main helper function for phoneme_syllabize
+		"""
 		def next_phoneme_is_vowel(phonemes, cur_i):
 			next_i = cur_i + 1
 			if next_i >= len(phonemes):
@@ -61,9 +36,7 @@ class Syllabizer:
 			else:
 				return self.is_vowel(phonemes[next_i])
 
-		phonemes = self.word_to_phonemes(s)
-
-		if self.count_number_of_syllables(s) == 1:
+		if self._count_number_of_syllables_in_phonemes(phonemes) == 1:
 			return [phonemes]
 
 		syllables = []
@@ -89,28 +62,66 @@ class Syllabizer:
 					prev_phoneme_is_vowel = False
 				else:
 					syllables.append(cur_syllable)
-					cur_syllable = []
-					while (i < len(phonemes)) and (not self.is_vowel(p)):	# Append until next vowel
-						cur_syllable.append(p)
+					cur_syllable = [p]
+					while (i + 1 < len(phonemes)) and (not self.is_vowel(p)):	# Append until next vowel
 						i += 1
 						p = phonemes[i]
-					cur_syllable.append(p)
+						cur_syllable.append(p)
+
+					if sum([1 for p in cur_syllable if self.is_vowel(p)]) == 0:	# Didn't hit any more vowels
+						syllables[-1] += cur_syllable 							# Test case: apologized
+						cur_syllable = []										# Clear so it gets filtered out
+
 					prev_phoneme_is_vowel = True
 			i += 1
 		syllables.append(cur_syllable)
 
 		# Filter out initial empty syllable, e.g. hello
-		syllables = [s for s in syllables if len(s) > 0]
+		syllables = [syl for syl in syllables if len(syl) > 0]
 
 		return syllables
 
-	def count_number_of_syllables(self, s):
+	def phoneme_syllabize(self, s):
 		"""
-		Return number of vowel phonemes
+		Given string, return phonemes grouped by syllable
+
+		Examples
+		--------
+		green -> [[u'G', u'R', u'IY1', u'N']]
+		hello -> [[u'HH', u'AH0'], [u'L', u'OW1']]
+		invisible -> [[u'IH0', u'N'], [u'V', u'IH1'], [u'Z', u'AH0'], [u'B', u'AH0', u'L']]
+		newspaper -> [[u'N', u'UW1', u'Z'], [u'P', u'EY2'], [u'P', u'ER0']]
+		explicit -> [[u'IH0', u'K'], [u'S', u'P', u'L', u'IH1'], [u'S', u'AH0', u'T']]
+		airplane -> [[u'EH1', u'R'], [u'P', u'L', u'EY2', u'N']]
+		idea -> [[u'AY0'], [u'D', u'IY1'], [u'AH0']]
+		apologized -> [[u'AH0'], [u'P', u'AA1'], [u'L', u'AH0'], [u'JH', u'AY2', u'Z', u'D']]
+
+		Pseudocode
+		----------
+		If word has only one syllable, return phonemes
+
+		If current phoneme is vowel:
+			If previous phoneme is vowel:
+				this is start of new syllable 
+			Else (previous phoneme is consonant):
+				this is part of previous syllable
+
+		Else (current phoneme is consonant):
+			If next phoneme is vowel:
+		      	this is start of new syllable
+		  	Else if previous phoneme is vowel:
+		      	this is part of previous syllable
+		  	Else (both previous and next aren't vowels), e.g. airplane EH1 R *P* L EY2 N; scogin *S* K OW1 G IH0 N 
+				this is start of new syllable
+				append until next vowel
+
+		Notes
+		-----
+		This can be used to build the context for linguistic features for a synthesizer. While a word is split into
+		the basic unit of a phoneme, we want to know for every phoneme, which syllable, word, and phrase it belongs to. 
 		"""
 		phonemes = self.word_to_phonemes(s)
-		return len([p for p in phonemes if self.is_vowel(p)])
-		
+		return self._phoneme_syllabize(phonemes)
 
 	def grapheme_syllabize(self,s):
 		"""
